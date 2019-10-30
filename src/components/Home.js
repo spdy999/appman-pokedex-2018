6bb779d7f9881987b432ed3370e3a2f4865e3ba1
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-10-30 22:27:36 +0700

    fetch data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
-const Home = () => <div>hello</div>;
+const Home = () => {
+  const [cards, setPokemon] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedPokemons = await Axios.get(
+        'http://localhost:3030/api/cards',
+      );
+      const cards = fetchedPokemons.data.cards;
+      setPokemon(cards);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {cards.map(pokemon => (
+        <li>{JSON.stringify(pokemon)}</li>
+      ))}
+    </div>
+  );
+};
 
 export default Home;
